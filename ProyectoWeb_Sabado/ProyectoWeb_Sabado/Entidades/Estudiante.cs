0d75edb7d5da71eafe7cfee62cffca777216c6e6
Alonso <91107183+Alonsoh301@users.noreply.github.com>
2024-04-26 18:33:08 -0600

    Update Estudiante.cs

--- ProyectoWeb_Sabado/ProyectoWeb_Sabado/Entidades/Estudiante.cs
+++ ProyectoWeb_Sabado/ProyectoWeb_Sabado/Entidades/Estudiante.cs
@@ -1,26 +1,22 @@
 ﻿namespace ProyectoMEP.Entidades
 {
-    public class Estudiante
-    {
-        public long IdEstudiante { get; set; }
-        public string? Correo { get; set; }
-        public string? Contrasenna { get; set; }
-        public string? NombreEstudiante { get; set; }
-        public short IdRol { get; set; }
-        public string? NombreRol { get; set; }
-        public bool Estado { get; set; }
-        public string? Token { get; set; }
-        public bool EsTemporal { get; set; }
-        public string? ContrasennaTemporal { get; set; }
-        public short IdCategoria { get; set; }
-        public string? NombreCategoria { get; set; }
+    
+        public class Usuario
+        {
+            public int id_usuario { get; set; }
+            public string nombre { get; set; }
+            public string correo { get; set; }
+            public string contrasena { get; set; }
+            public bool Estado { get; set; }
+            public string rol { get; set; }
+        
     }
 
-    public class EstudianteRespuesta
+    public class UsuarioRespuesta
     {
         public string? Codigo { get; set; }
         public string? Mensaje { get; set; }
-        public Estudiante? Dato { get; set; }
-        public List<Estudiante>? Datos { get; set; }
+        public Usuario? Dato { get; set; }
+        public List<Usuario>? Datos { get; set; }
     }
 }
